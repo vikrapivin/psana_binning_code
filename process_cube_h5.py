1d142e304843af92134eda1dde5b92f85b4b268d
Viktor <10377552+vikrapivin@users.noreply.github.com>
2021-05-08 17:16:24 -0700

    combined laser off processing into the same file. Fixed a condition on the thresholding.

--- process_cube_h5.py
+++ process_cube_h5.py
@@ -24,9 +24,11 @@ print(f'rank {rank} out of {size}')
 parser = argparse.ArgumentParser()
 parser.add_argument("run", help="run number or range, e.g. 100,109-113.", type=str)
 parser.add_argument("--num_events", help="number of events to process", type=int, default=1<<31)
+parser.add_argument("--laser_off", help="whether or not to add a laser off cube to the cube", type=int, default=0)
 args = parser.parse_args()
 run_num = args.run
 num_events_limit = args.num_events
+process_laser_off = args.laser_off
 
 ########## set parameters here: #################
 expname = 'xpplv2818'
@@ -81,12 +83,14 @@ encoder_src = psana.Detector('XPP-USB-ENCODER-02')
 #encoder_src = psana.Detector()
 #encoder_src = '';
 
-bin_delays = binEvents(delay_range_lower, delay_range_upper, num_bins)
 bin_ipm2 = binEvents(delay_range_lower, delay_range_upper, num_bins)
 bin_ipm3 = binEvents(delay_range_lower, delay_range_upper, num_bins)
-bin_ttdelay = binEvents(delay_range_lower, delay_range_upper, num_bins)
 bin_cspad_sum = binEvents(delay_range_lower, delay_range_upper, num_bins)
-# cspad_stats = collect_stats()
+if process_laser_off !=0:
+  bin_cspad_sum_off = binEvents(delay_range_lower, delay_range_upper, num_bins)
+  bin_ipm2_off = binEvents(delay_range_lower, delay_range_upper, num_bins)
+  bin_ipm3_off = binEvents(delay_range_lower, delay_range_upper, num_bins)
+
 
 def mpi_message(msg):
   if rank == 0:
@@ -138,7 +142,9 @@ def filter_events(evts):
       print("*** no evr, skipping.")
       continue
     evr_list = [x.eventCode() for x in evr.fifoEvents()]
-    if evr_list.count(evr2skip)>0:
+    if process_laser_off !=0:
+      pass
+    elif evr_list.count(evr2skip)>0:
       # print('case evr_list')
       skipctr += 1
       continue
@@ -223,7 +229,8 @@ def get_config_string():
   weightby = {}
   thresholdVal = {}
   thresholdVal_max = {}
-    """
+  process_laser_off = {}
+  """
   s = msg.format( expname,
                   savepath,
                   ipmlower,
@@ -239,7 +246,8 @@ def get_config_string():
                   num_bins,
                   weightby,
                   thresholdVal,
-                  thresholdVal_max)
+                  thresholdVal_max,
+                  process_laser_off)
   return s
 
 
@@ -312,32 +320,62 @@ for nevent, ev in enumerate(filter_events(ds.events() )):
     print("*** missing cspad data. Skipping event...")
     continue
   else:
-    cspad_data[cspad_data <= thresholdVal] = 0
-    cspad_data[cspad_data <= thresholdVal_max] = 0
-    bin_cspad_sum.update_bins(delay, cspad_data)
-    bin_ipm2.update_bins(delay, ipm2intens)
-    bin_ipm3.update_bins(delay, ipm3intens)
+    evr=ev.get(psana.EvrData.DataV4, evr_src)
+    evr_list = [x.eventCode() for x in evr.fifoEvents()]
+    if evr_list.count(laseroffevr)>0:
+      cspad_data[cspad_data <= thresholdVal] = 0
+      cspad_data[cspad_data >= thresholdVal_max] = 0
+      bin_cspad_sum_off.update_bins(delay, cspad_data)
+      bin_ipm2_off.update_bins(delay, ipm2intens)
+      bin_ipm3_off.update_bins(delay, ipm3intens)
+    elif evr_list.count(laseronevr)>0:
+      cspad_data[cspad_data <= thresholdVal] = 0
+      cspad_data[cspad_data >= thresholdVal_max] = 0
+      bin_cspad_sum.update_bins(delay, cspad_data)
+      bin_ipm2.update_bins(delay, ipm2intens)
+      bin_ipm3.update_bins(delay, ipm3intens)
   # print(f'# events: {nevent}')
   if (total_events%100==0):
     # print(f'# events: {nevent}')
     mpi_message("# events: %s"% nevent)
-bin_cspad_mean2 = np.copy(bin_cspad_sum._bin_count)
-comm.Reduce(bin_cspad_sum._bin_count, bin_cspad_mean2)
-bin_cspad_mean, bin_cspad_sum_count, bin_ipm2_mean, bin_ipm2_cts, bin_ipm3_mean, bin_ipm3_cts = mpi_reduce_arrays(bin_cspad_sum._img, 
+# bin_count_cspad = np.copy(bin_cspad_sum._bin_count)
+# comm.Reduce(bin_cspad_sum._bin_count, bin_count_cspad)
+bin_cspad_mean, bin_cspad_sum_count, bin_ipm2_mean, bin_ipm2_cts, bin_ipm3_mean, bin_ipm3_cts = mpi_reduce_arrays(
+                    bin_cspad_sum._img, 
                     bin_cspad_sum._bin_count, 
                     bin_ipm2._img, 
                     bin_ipm2._bin_count,
                     bin_ipm3._img, 
                     bin_ipm3._bin_count)
+if process_laser_off !=0:
+  bin_cspad_mean_off, bin_cspad_sum_count_off, bin_ipm2_mean_off, bin_ipm2_cts_off, bin_ipm3_mean_off, bin_ipm3_cts_off = mpi_reduce_arrays(
+                      bin_cspad_sum_off._img, 
+                      bin_cspad_sum_off._bin_count, 
+                      bin_ipm2_off._img, 
+                      bin_ipm2_off._bin_count,
+                      bin_ipm3_off._img, 
+                      bin_ipm3_off._bin_count)
 
 delays = bin_cspad_sum.bin_edges()
-print(bin_cspad_mean2)
+mpi_message(bin_cspad_sum_count)
 if rank==0:
-   saveh5(savepath,  cspad = bin_cspad_mean, 
-                     nEntries = bin_cspad_sum_count, 
-                     ipm2_sum = bin_ipm2_mean, 
-                     ipm3_sum = bin_ipm3_mean, 
-                     bins = delays,
-                     config = get_config_string())
-   print("****** Done. ")
+  if process_laser_off == 0:
+    saveh5(savepath,  cspad = bin_cspad_mean, 
+                      nEntries = bin_cspad_sum_count, 
+                      ipm2_sum = bin_ipm2_mean, 
+                      ipm3_sum = bin_ipm3_mean, 
+                      bins = delays,
+                      config = get_config_string())
+  elif process_laser_off != 0:
+    saveh5(savepath,  cspad = bin_cspad_mean, 
+                      nEntries = bin_cspad_sum_count, 
+                      ipm2_sum = bin_ipm2_mean, 
+                      ipm3_sum = bin_ipm3_mean, 
+                      cspad_off = bin_cspad_mean_off, 
+                      nEntries_off = bin_cspad_sum_count_off, 
+                      ipm2_sum_off = bin_ipm2_mean_off, 
+                      ipm3_sum_off = bin_ipm3_mean_off, 
+                      bins = delays,
+                      config = get_config_string())
+  print("****** Done. ")
 
